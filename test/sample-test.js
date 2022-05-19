42bd72ca609a540769eed9df4e4c9738a5db43a0
md-maruf-hossain <sany.maruf@gmail.com>
2022-05-19 17:26:56 +0600

    testing done

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,43 @@
-const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function() {
+  it("Should create and execute market sales", async function() {
+    // Deploy the marketplace
+    const BanglaRarify = await ethers.getContractFactory("BanglaRarify");
+    const banglaRarify = await BanglaRarify.deploy();
+    await banglaRarify.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    let listingPrice = await banglaRarify.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether');
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // Creating two tokens
+    await banglaRarify.createToken("https://www.banglatoken.com", auctionPrice, {value: listingPrice});
+    await banglaRarify.createToken("https://www.banglatoken2.com", auctionPrice, {value: listingPrice});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    const [_, buyerAddress] = await ethers.getSigners();
+
+    // Executing sale of token to another user
+    await banglaRarify.connect(buyerAddress).createMarketSale(1, { value: auctionPrice });
+
+    // Reselling a token
+    await banglaRarify.connect(buyerAddress).resellToken(1, auctionPrice, { value: listingPrice });
+
+    // query for and returning the unsold items
+    items = await banglaRarify.fetchMarketItems();
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await banglaRarify.tokenURI(i.tokenId);
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+
+  })
+})
