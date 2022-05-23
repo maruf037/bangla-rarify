4d2f51df6e8baedaad14f9cb497946d7b38b14da
md-maruf-hossain <sany.maruf@gmail.com>
2022-05-23 16:06:48 +0600

    "create-nft" page created

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Web3mModal from 'web3modal'
+import Web3Modal from 'web3modal'
 
 import { marketplaceAddress } from '../config'
 
@@ -42,13 +42,13 @@ export default function Home() {
   }
   async function buyNft(nft) {
     // Needs the user to sign the transaction, so we will use Web3Provider and sign it
-    const Web3Modal = new Web3Modal()
-    const connection = await Web3Modal.connect()
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(marketplaceAddress, BanglaRarify.abi, signer)
 
-    // user will be prompted to pay the asking process to complete the transaction
+    // user will be prompted to pay the asking price to complete the transaction
     const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
     const transaction = await contract.createMarketSale(nft.tokenId, {
       value: price
